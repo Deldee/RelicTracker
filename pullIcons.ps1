7592866cd70d9efbd9b416cad24d309a0c64d015
Gabriel Laplante <glaplante@thecloudconnectors.com>
2026-03-24 11:01:40 -0400

    Added item icons

---
+++ pullIcons.ps1
@@ -0,0 +1,12 @@
+﻿$outFolder = "G:\FFXIVmods\RelicTracker\resources\img\itemIcon"
+$text = '+Name~"Crystal Paste"' 
+$query = [System.Net.WebUtility]::UrlEncode($text)
+#Get all Atmas
+$content = Invoke-RestMethod "https://v2.xivapi.com/api/search?query=$query&sheets=Item&fields=Name%2CIcon.path" | Select-Object results
+foreach ($result in $content.results){
+$path = $result.fields.Icon.path
+$name = $result.fields.Name -replace " ", "_" -replace ":",""
+Invoke-WebRequest -Uri "https://v2.xivapi.com/api/asset?path=$path&format=png" -OutFile "$outFolder\$name.png"
+}
+
+
